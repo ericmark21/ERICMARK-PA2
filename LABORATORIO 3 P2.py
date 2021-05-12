210ca0f7d3724ff36b16abc4ae772004e57a56b8
eric mark <eric.mark@utp.ac.pa>
2021-05-11 22:32:12 -0500

    REGISTRO DE VEHICULOS POR LISTA

---
+++ LABORATORIO 3 P2.py
@@ -0,0 +1,31 @@
+
+print("Bienvenido al programa\n")
+print("REGISTRO DE VEHICULOS DE TRES EJES \n")
+cont=0 #contador para vehiculos de mayor peso de 22 toneladas
+c=0 #contador para division
+promedio=0
+suma=0
+lista=[]
+while (True):
+    try:
+        #peso= float(input("Registre el peso de su vehiculo en toneladas: "))
+        while (True):
+            peso= float(input("Registre el peso de su vehiculo en toneladas: "))
+            if (peso ==0): 
+                break
+            else:
+                lista.append(peso)
+                suma+= peso
+                c+= 1
+                if(peso>=22):
+                    cont+= 1
+        promedio=sum(lista)/len(lista)
+                       
+    except:
+        print("Ha ocurrido un error, digite solo numeros, no palabras")
+    else:
+        print("Lista de los pesos ingresados: ")
+        print(lista)
+        print(f"el promedio es: {promedio}")
+        print(f"Vehiculos quesobrepasan las 22 toneladas: {int(cont)}")
+        
